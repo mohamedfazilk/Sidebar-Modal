2e0113b7bef201cfb437f636a8625f022dab8c8c
mohamedfazilk <2114921@cognizant.com>
2022-07-26 11:25:05 +0530

    import the useGlobalContext into Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,8 +2,12 @@ import React from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
+import { useGlobalContext } from './context'
+
+
 
 const Sidebar = () => {
+  
   return <aside className={`sidebar show-sidebar`}>
       <div className="sidebar-header">
           <img src={logo} alt="codding-addict" />
