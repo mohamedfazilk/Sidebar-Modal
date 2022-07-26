873b9d15aa48ae08a0c679c5ec6148a866f72695
mohamedfazilk <2114921@cognizant.com>
2022-07-26 10:53:39 +0530

    two state values for sidebar and modal

--- src/context.js
+++ src/context.js
@@ -3,6 +3,11 @@ import React, { useState, useContext } from 'react'
 const AppContext = React.createContext();  //creating a provider context
 
 const AppProvider = ({children}) =>{
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false); //for opening & closing sidebar
+    const [isModalOpen, setIsModalOpen] = useState(false);     //for opening & closing modal
+    
+    
+
     return <AppContext.Provider  value='hello'>{children}</AppContext.Provider>
 }
 
