7749c53a18144abdad767ddc557942217f1fa213
mohamedfazilk <2114921@cognizant.com>
2022-07-26 11:00:44 +0530

    Create four functions openSidebar,closesSidebar,openModal,closeModal

--- src/context.js
+++ src/context.js
@@ -5,6 +5,23 @@ const AppContext = React.createContext();  //creating a provider context
 const AppProvider = ({children}) =>{
     const [isSidebarOpen, setIsSidebarOpen] = useState(false); //for opening & closing sidebar
     const [isModalOpen, setIsModalOpen] = useState(false);     //for opening & closing modal
+
+    const openSidebar = () =>{
+        setIsSidebarOpen(true)
+    }
+
+    const closeSidebar = () =>{
+        setIsSidebarOpen(false)
+    }
+    
+
+    const openmodal = () =>{
+        setIsModalOpen(true)
+    }
+
+    const closemodal = () =>{
+        setIsModalOpen(false)
+    }
     
     
 
